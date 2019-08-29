7cbdade0a397655e7c0a42c88c5c5d1cc817d60b
Jim Farrell <jimmyfarrell007@gmail.com>
2019-08-28 21:13:20 -0400

    few comments

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+#######################################################################
+#Class: SWDV-660
+#Assignment: Week 1 Package Managed Projects
+#Name: Jim Farrell
+#Description: Create a spinning / to demonstrate halo spinner.
+#######################################################################
 import time
 from colorama import init, Fore, Back
 from halo import Halo
 
-init()
+def main():
+    init() #initialize colorama
+    print( Back.BLUE + Fore.RED + " halo spinner example ")
+    #create the Halo spinner
+    spinner = Halo(text='What is taking so long..', text_color='cyan', color='green')
+    #start the spin simulation
+    spinner.start()
+    #sleep
+    time.sleep(5)
+    #stop and print the final message
+    spinner.stop_and_persist(symbol='✔ '.encode('utf-8'), text='Done!')
 
-print( Back.BLUE + Fore.RED + " halo spinner example ")
-spinner = Halo(text='What is taking so long..', text_color='cyan', color='green')
-spinner.start()
-time.sleep(5)
-spinner.stop_and_persist(symbol='✔ '.encode('utf-8'), text='Done!')
+main()
